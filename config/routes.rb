fc82d4f75dfde9f30fba1b767c2b6a4b0049ca86
youyou1605 <64137553+youyou1605@users.noreply.github.com>
2022-12-16 17:54:45 +0000

    [Made]users show,edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-    resources :books, only: [:new, :index, :show]
   devise_for :users
   root to: 'homes#top'
+  resources :users,only: [:index, :show, :edit, :update]
+  resources :books, only: [:create, :index, :show, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
