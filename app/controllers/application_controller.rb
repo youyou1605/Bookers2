345c6554c8f11f94861311ba58bf0edd85436f92
youyou1605 <64137553+youyou1605@users.noreply.github.com>
2022-12-20 13:45:57 +0000

    [Done]Bootstop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,5 +17,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
   end
 end
